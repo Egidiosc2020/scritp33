293500bfd08ae5502282ba8d95ae6ba032288bb5
aluno <aluno@Lab-24>
2020-03-05 10:50:34 -0300

    Comit mensagem com echo

--- teste.sh
+++ teste.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Autor: Egídio
+#Autor: Egídio Neto
 #To lascado
 
 cd imagens-livros
@@ -9,6 +9,6 @@ echo $imagem
 img_sem_ext=$(ls $imagem.jpg | awk -F. '{print $1}')
 convert $img_sem_ext &imagem.png
 done
-
+echo "iniciando"
 
 cd ..
